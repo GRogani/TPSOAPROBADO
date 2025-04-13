0463bb3e71c945917d2d1d5c7e8ad23092b1f791
GRogani <groganicano@frba.utn.edu.ar>
2025-04-13 18:47:51 +0000

    Funciones para serializacion

---
+++ utils/src/utils/protocolo.c
@@ -0,0 +1,81 @@
+#include "protocolo.h"
+
+
+//FUNCIONES PARA SERIALIZAR
+t_buffer *buffer_create(uint32_t size){
+    t_buffer *buffer = malloc(sizeof(t_buffer));
+    buffer->offset = 0;
+    buffer->size = size;
+    buffer->stream = malloc(buffer->size);
+    return buffer;
+}
+
+void buffer_destroy(t_buffer *buffer){
+    free(buffer->stream);
+    free(buffer);
+}
+
+void buffer_add(t_buffer *buffer, void *data, uint32_t tamanio){
+    void *new_stream = realloc(buffer->stream, buffer->size + tamanio);
+	if (new_stream == NULL)
+	{
+		return;
+	}
+	buffer->stream = new_stream;
+	memcpy(buffer->stream + buffer->offset, data, tamanio);
+	buffer->size += tamanio;
+	buffer->offset += tamanio;
+}
+
+void buffer_add_uint32(t_buffer *buffer, uint32_t data){
+    buffer_add(buffer, &data, sizeof(uint32_t));
+}
+
+void buffer_add_string(t_buffer *buffer, uint32_t length, char *string){
+    buffer_add_uint32(buffer, length);
+    buffer_add(buffer, string, length);
+}
+
+void buffer_add_pointer(t_buffer *buffer, void *ptr) {
+    uintptr_t ptr_as_integer = (uintptr_t)ptr; // Convertir el puntero a un entero sin signo
+    buffer_add(buffer, &ptr_as_integer, sizeof(uintptr_t)); // Agregar al buffer
+}
+
+//FUNCIONES PARA DESERIALIZAR
+void buffer_read(t_buffer *buffer, void *data, uint32_t size){
+   	if (buffer == NULL || data == NULL)
+	{
+		return;
+	}
+
+	// Verificar límites de lectura
+	if (buffer->offset + size > buffer->size)
+	{
+		return;
+	}
+
+	memcpy(data, buffer->stream + buffer->offset, size);
+
+	// Actualizar el desplazamiento del buffer
+	buffer->offset += size;
+}
+
+uint32_t buffer_read_uint32(t_buffer *buffer){
+    uint32_t data;
+    buffer_read(buffer, &data, sizeof(uint32_t));
+    return data;
+}
+
+char *buffer_read_string(t_buffer *buffer, uint32_t *length){
+    *length = buffer_read_uint32(buffer);
+    char *string = malloc(*length);
+    buffer_read(buffer, string, *length);
+    return string;
+}
+
+void* buffer_read_pointer(t_buffer *buffer) {
+    uintptr_t ptr_as_integer;
+    buffer_read(buffer, &ptr_as_integer, sizeof(uintptr_t)); // Leer el puntero como entero
+    return (void*)ptr_as_integer; // Convertir de vuelta a un puntero
+}
+
