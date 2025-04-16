84916836a7d6f573854530c4f5bf30ac9948614a
gerofayo <gfayo@frba.utn.edu.ar>
2025-04-16 19:53:39 +0000

    Generalizacion de las configs para todos los modulos.

--- kernel/src/config/config.h
+++ kernel/src/config/config.h
@@ -12,14 +12,14 @@
 #include "enum/Eplanification_algorithm.h"
 
 
-typedef struct {
-	char* port;
-    char* memory_ip;
-    char* memory_port;
-    PLANIFICATION_ALGORITHM planification_algorithm;
-    int sleep_time;
-    t_log_level log_level;
-} t_kernel_config;
+// typedef struct t_kernel_config{
+// 	char* port;
+//     char* memory_ip;
+//     char* memory_port;
+//     PLANIFICATION_ALGORITHM planification_algorithm;
+//     int sleep_time;
+//     t_log_level log_level;
+// } t_kernel_config;
 
 t_config* init_config_and_validate(t_kernel_config* kernel_config);
 
