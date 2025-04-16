2f0596d6af3ce29a6dfba9c549e4db304196b69f
gerofayo <gfayo@frba.utn.edu.ar>
2025-04-16 20:08:41 +0000

    Reorganizacion de archivos.

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,24 +1,8 @@
-#include "config/config.h"
-#include "utils/config/config.h"
-#include "utils/logger/logger.h"
-#include "utils/socket/server.h"
-#include "utils/safe_alloc.h"
-
-void shutdown_hook(t_kernel_config kernel_config, t_config* config) {
-    log_info(get_logger(), "Shuting down...");
-    free(kernel_config.memory_port);
-    free(kernel_config.port);
-    free(kernel_config.memory_ip);
-    config_destroy(config);
-    destroy_logger();
-}
+#include "main.h"
 
 int main(int argc, char* argv[]) {
     
     //=================INIT CONFIG/LOGGER===============
-    //t_kernel_config* kernel_config = safe_malloc(sizeof(t_kernel_config));
-
-    //t_config* config = init_config_and_validate(kernel_config);
 
     t_config *config_file = init_config("kernel.config");
     t_kernel_config kernel_config = init_kernel_config(config_file);
@@ -32,7 +16,7 @@ int main(int argc, char* argv[]) {
 
     int socket_client = accept_connection(socket_server);
 
-    shutdown_hook(kernel_config, config_file);
+    shutdown_kernel(kernel_config, config_file);
 
     return 0;
 }
