aa51b8d2e47c0e037ca531523e58efe835b9101b
gerofayo <gfayo@frba.utn.edu.ar>
2025-04-29 15:17:43 +0000

    Bosquejo memoria

--- kernel/src/handlers/io_client.c
+++ kernel/src/handlers/io_client.c
@@ -7,17 +7,19 @@ void* handle_io_client(void* socket)
     while (1)
     {
         package = recv_package(*client_socket);
-
-        switch(package->opcode)
+        if(package)
         {
-            case HANDSHAKE: //func1();
-                break;
-
-            default:
+            switch(package->opcode)
             {
-                pthread_exit(0);
-                close(*client_socket); 
-                break;
+                case HANDSHAKE: LOG_DEBUG("Handshake recibido");
+                    break;
+
+                default:
+                {
+                    pthread_exit(0);
+                    close(*client_socket); 
+                    break;
+                }
             }
         }
     }
