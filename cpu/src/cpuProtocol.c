ce19056d22ddcefadabe1d07da119b708bb71b6e
gerofayo <gfayo@frba.utn.edu.ar>
2025-05-27 18:15:22 +0000

    Agrego PID al mensaje que envia el CPU

--- cpu/src/cpuProtocol.c
+++ cpu/src/cpuProtocol.c
@@ -1,9 +1,12 @@
 #include "cpuProtocol.h"
 
-t_package* receive_PID_PC_Package(int socket_dispatch_kernel) {
+t_package* receive_PID_PC_Package(int socket_dispatch_kernel, uint32_t* PID, uint32_t* PC) 
+{
+
     t_package* package = recv_package(socket_dispatch_kernel);
+
     if (package == NULL) {
-       log_error(get_logger(), "Failed to receive package");
+       log_error(get_logger(), "Disconnected from kernel");
         return NULL;
     }
 
@@ -13,26 +16,36 @@ t_package* receive_PID_PC_Package(int socket_dispatch_kernel) {
         return NULL;
     }
 
+    package->buffer->offset = 0;
+    *PID = buffer_read_uint32(package->buffer);
+    *PC = buffer_read_uint32(package->buffer);
+
     return package;
 }
 
-void request_instruction(int socket, int PC) {
-    t_buffer* buffer = buffer_create(sizeof(uint32_t));
+void request_instruction(int socket, uint32_t PID, uint32_t PC) 
+{
+    t_buffer* buffer = buffer_create(sizeof(uint32_t)*2);
+    buffer_add_uint32(buffer, PID);
     buffer_add_uint32(buffer, PC);
+
     t_package* package = package_create(CPU_REQUEST_INSTRUCTION, buffer);
+
     send_package(socket, package);
     package_destroy(package);
 }
 
-t_package* receive_instruction(int socket) {
+t_package* receive_instruction(int socket) 
+{
     t_package* package = recv_package(socket);
+
     if (package == NULL) {
-        log_error(get_logger(), "Failed to receive package");
+        log_error(get_logger(), "Failed to receive instruction package");
         return NULL;
     }
 
     if (package->opcode != CPU_RESPONSE_INSTRUCTION) {
-        log_error(get_logger(), "Received package with unexpected opcode: %d", package->opcode);
+        log_error(get_logger(), "Received instruction package with unexpected opcode: %d", package->opcode);
         package_destroy(package);
         return NULL;
     }
@@ -40,19 +53,23 @@ t_package* receive_instruction(int socket) {
     return package;
 }
 
-void write_memory_request(int socket_memory, uint32_t direccion_fisica, char* valor_write) {
+void write_memory_request(int socket_memory, uint32_t direccion_fisica, char* valor_write) 
+{
     t_buffer* buffer = buffer_create(sizeof(uint32_t) + strlen(valor_write) + 1);
     buffer_add_uint32(buffer, direccion_fisica);
     buffer_add_string(buffer, strlen(valor_write), valor_write);
+
     t_package* package = package_create(CPU_WRITE_MEMORY_REQUEST, buffer);
     send_package(socket_memory, package);
     package_destroy(package);
 }
 
-void read_memory_request(int socket_memory, uint32_t direccion_fisica, uint32_t size) {
+void read_memory_request(int socket_memory, uint32_t direccion_fisica, uint32_t size) 
+{
     t_buffer* buffer = buffer_create( 2 * sizeof(uint32_t));
     buffer_add_uint32(buffer, direccion_fisica);
     buffer_add_uint32(buffer, size);
+
     t_package* package = package_create(CPU_READ_MEMORY_REQUEST, buffer);
     send_package(socket_memory, package);
     package_destroy(package);
@@ -61,54 +78,42 @@ void read_memory_request(int socket_memory, uint32_t direccion_fisica, uint32_t 
 char* read_memory_response(int socket_memory) {
     t_package* package = recv_package(socket_memory);
     if (package == NULL) {
-       log_error(get_logger(), "Failed to receive package");
+       log_error(get_logger(), "Failed to receive Read response from memory");
         return NULL;
     }
 
     if (package->opcode != CPU_READ_MEMORY_RESPONSE) {
-       log_error(get_logger(), "Received package with unexpected opcode: %d", package->opcode);
+       log_error(get_logger(), "Received memory response with unexpected opcode: %d", package->opcode);
         package_destroy(package);
         return NULL;
     }
 
-    char* data = buffer_read_string(package->buffer, package->buffer->stream_size);
+    package->buffer->offset = 0;
+    uint32_t bytes_read = 0;
+    char* data = buffer_read_string(package->buffer, &bytes_read);
 
     package_destroy(package);
+    
     return data;
 }
 
-t_package* recv_interrupt_package(int socket_interrupt) {
-OPCODE opcode;
-    uint32_t buffer_stream_size;
+void create_connections(t_cpu_config config_cpu, int* fd_memory, int* fd_kernel_dispatch, int* fd_kernel_interrupt)
+{
+    while( (*fd_memory < 0) && (*fd_kernel_dispatch < 0) && (*fd_kernel_interrupt < 0) )
+    {
+        if(*fd_memory < 0)
+            *fd_memory = create_connection(config_cpu.PUERTO_MEMORIA, config_cpu.IP_MEMORIA);
 
-    // recibir opcode
-    // aca ya se puede validar si vale la pena seguir solo con el opcode
-    if (recv(socket, &opcode, sizeof(OPCODE), MSG_DONTWAIT) <= 0) { 
-        return NULL; 
+        if(*fd_kernel_dispatch < 0)    
+            *fd_kernel_dispatch = create_connection(config_cpu.PUERTO_KERNEL_DISPATCH, config_cpu.IP_KERNEL);
+
+        if(*fd_kernel_interrupt < 0)
+            *fd_kernel_interrupt = create_connection(config_cpu.PUERTO_KERNEL_INTERRUPT, config_cpu.IP_KERNEL);
+
+        if( (*fd_memory < 0) && (*fd_kernel_dispatch < 0) && (*fd_kernel_interrupt < 0) )
+        {
+            log_info(get_logger(), "Retrying connections...");
+            sleep(3);
+        }
     }
-
-    // recibir el buffer size
-    // quiza es muy grande se puede validar eso
-    if (recv(socket, &buffer_stream_size, sizeof(uint32_t), MSG_DONTWAIT) <= 0) {
-        return NULL; 
-    }
-
-    // hago espacio para buffer serializado
-    void* buffer_stream_data = safe_malloc(buffer_stream_size);
-
-    // guardo el buffer stream de data serializado / caso error libero memoria
-    if (recv(socket, buffer_stream_data, buffer_stream_size, MSG_DONTWAIT) <= 0) {
-        free(buffer_stream_data); 
-        return NULL;
-    }
-
-    t_buffer* buffer = buffer_create(buffer_stream_size); // Crea un buffer con el tamaño recibido
-    buffer_add(buffer, buffer_stream_data, buffer_stream_size); // Agrega los datos serializados al buffer
-
-    t_package* package = package_create(opcode, buffer);
-
-    free(buffer_stream_data); // liberar memoria de los datos serializados
-
-    return package;
-
 }
