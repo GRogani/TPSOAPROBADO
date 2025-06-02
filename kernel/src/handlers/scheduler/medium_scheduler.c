685ad6854460984a7c15cb82b88771463a13314d
Alaparamos <alaparamos@frba.utn.edu.ar>
2025-06-02 01:15:12 +0000

    Camino Feliz + TODO validaciones + Falta Test

--- kernel/src/handlers/scheduler/medium_scheduler.c
+++ kernel/src/handlers/scheduler/medium_scheduler.c
@@ -12,32 +12,40 @@ void run_medium_scheduler(uint32_t  pid, uint32_t timer){
    * 6. si no existe, loggeamos y no hacemos nada (probablemente ya se resolvió el proceso)
    */
 
-    // TODO: usar el nuevo looger
-    log_info(get_logger(), "MEDIUM_SCHEDULER: Tiempo de espera de [%d] para PID [%u]", timer, pid);
+    LOG_INFO("MEDIUM_SCHEDULER: Tiempo de espera de [%d] para PID [%u]", timer, pid);
     sleep(timer);
     lock_blocked_list();
     if(!find_pcb_in_blocked(pid)) {
         unlock_blocked_list();
-        log_info(get_logger(), "MEDIUM_SCHEDULER: PID [%d] no está en BLOCKED", pid);
-        return NULL;
+        LOG_INFO("MEDIUM_SCHEDULER: PID [%d] no está en BLOCKED.", pid);
+        return;
     }
-    unlock_blocked_list();
     // BLOCKED -> BLOCKED_SUSPEND
     t_pcb* pcb = remove_pcb_from_blocked(pid);
-    pcb->current_state = SUSP_BLOCKED;
     unlock_blocked_list();
-    log_info(get_logger(), "MEDIUM_SCHEDULER: Cambio de Estado BLOCKED->SUSPENDED_BLOCKED del PID = %d.", pid);
+    pcb->current_state = SUSP_BLOCKED;
+    LOG_INFO("MEDIUM_SCHEDULER: Cambio de Estado BLOCKED->SUSPENDED_BLOCKED del PID = %d.", pid);
 
     int memory_socket = connect_to_memory(&kernel_config);
     if (memory_socket == -1){
-        log_error(get_logger(), "MEDIUM_SCHEDULER: No se pudo conectar con Memoria para suspender PID = %d.", pid);
-        return NULL;
+        LOG_INFO("MEDIUM_SCHEDULER: No se pudo conectar con Memoria para suspender PID = %d.", pid);
+        return;
     }
-    log_info(get_logger(), "MEDIUM_SCHEDULER: Solicitando Servicio de Swapping para suspender PID = %d.", pid);
+    LOG_INFO("MEDIUM_SCHEDULER: Solicitando Servicio de Swapping para suspender PID = %d.", pid);
     // TODO: la memoria conoce a todos los procesos o lo tengo que mandar el PCB?
     send_memory_suspend_process(memory_socket, pid);
 
-    // TODO: espero la respuseta (qué me tendría que llegar)?
+    // TODO: espero la respuesta (qué me tendría que llegar)?
+    t_package* package; = recv_package(memory_socket);
+    if(package == NULL){
+        LOG_ERROR("MEDIUM_SCHEDULER: Falló la conexión con Memoria.");
+        return;
+    }
+    if(!read_memory_suspend_process_response(package)) {
+        LOG_INFO("MEDIUM_SCHEDULER: Falló el SWAP [%d]", pid);
+        return;
+    }
+    LOG_INFO("MEDIUM_SCHEDULER: Swap Exitoso del PID [%d]", pid);
     disconnect_from_memory(memory_socket);
 
     lock_susp_blocked_list();
@@ -45,9 +53,8 @@ void run_medium_scheduler(uint32_t  pid, uint32_t timer){
     unlock_susp_blocked_list();
 
     // Llamo al Planificador de Largo Plazo
-    log_info(get_logger(), "MEDIUM_SCHEDULER: Llamando a planificador de largo plazo para admitir nuevos procesos.");
-    if (!run_long_scheduler()) {
-        LOG_ERROR("MEDIUM_SCHEDULER: El planificador de largo plazo no puede admitir nuevos procesos.");
-    }
-    log_info(get_logger(), "MEDIUM_SCHEDULER: Finalización del PID [%d].", pid);
+    LOG_INFO("MEDIUM_SCHEDULER: Llamando a planificador de largo plazo para admitir nuevos procesos.");
+    if (!run_long_scheduler()) LOG_INFO("MEDIUM_SCHEDULER: El planificador de largo plazo no puede admitir nuevos procesos.");
+    // TODO: qué hago si no puede admitur nuevos procesos? Tenemos prioridad de esta cola sobre la de Ready, no?
+    LOG_INFO("MEDIUM_SCHEDULER: El planificador de largo plazo admitió a un nuevo PID [%d].", pid)
 }
--- utils/src/utils/DTOs/memory_suspend_process.c
+++ utils/src/utils/DTOs/memory_suspend_process.c
@@ -1,5 +1,6 @@
 #include "memory_suspend_process.h"
 
+// REQUEST de KERNEL
 int send_memory_suspend_process(int socket, uint32_t pid) {
     t_package* package = create_memory_suspend_process(pid);
     int bytes_sent = send_package(socket, package);
@@ -12,4 +13,31 @@ t_package *create_memory_suspend_process(uint32_t pid){
     t_buffer* buffer = buffer_create(buffer_size);
     buffer_add_uint32(buffer, pid);
     return package_create(SUSP_BLOCKED, buffer);
+}
+
+uint32_t read_memory_suspend_process(t_package* package) { // Lectura para Memoria
+    package->buffer->offset = 0;
+    uint32_t pid = buffer_read_uint32(package->buffer);
+    return pid;
+}
+
+// RESPONSE de MEMORIA
+int send_memory_suspend_process_response(int socket_fd, uint32_t success) {
+    t_package* package = create_memory_suspend_process_response(success);
+    int bytes_sent = send_package(socket_fd, package);
+    if (bytes_sent < 0) LOG_ERROR("DTO: Error al enviar paquete SUSPEND_PROCESS_RESPONSE a Kernel");
+    package_destroy(package);
+}
+
+t_package* create_memory_suspend_process_response(uint32_t success) {
+    uint32_t buffer_size = sizeof(uint32_t);
+    t_buffer* buffer = buffer_create(buffer_size);
+    buffer_add_uint32(buffer, success);
+    return package_create(SUSP_BLOCKED, buffer);
+}
+
+bool read_memory_suspend_process_response(t_package* package) { // Lectura para el KERNEL
+    package->buffer->offset = 0;
+    uint32_t success = buffer_read_uint32(package->buffer);
+    return success == 1;
 }
