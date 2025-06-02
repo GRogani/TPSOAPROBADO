52698b21816c962befe83e59ef9f9eadc5795105
gerofayo <gfayo@frba.utn.edu.ar>
2025-06-02 14:28:47 +0000

    Exit-syscall

--- kernel/src/handlers/cpu/exit_syscall.c
+++ kernel/src/handlers/cpu/exit_syscall.c
@@ -25,13 +25,14 @@ void exit_process_syscall(uint32_t pid)
 
 void log_process_metrics(uint32_t pid, t_state_metrics state_metrics, t_time_metrics time_metrics)
 {
-  LOG_INFO("## (%d) - Metricas de estado: NEW (%d) (%dms), READY (%d) (%dms), EXEC (%d) (%dms), BLOCK (%d) (%dms), SUSPEND (%d) (%dms), EXIT (%d) (%dms)", 
+  LOG_INFO("## (%d) - Metricas de estado: NEW (%d) (%dms), READY (%d) (%dms),  SUSPEND REDY (%d) (%dms), EXEC (%d) (%dms), BLOCK (%d) (%dms), SUSPEND BLOCK (%d) (%dms), EXIT (%d) (%dms)", 
           pid, 
-          state_metrics.new_count, time_metrics.new_time_ms, 
-          state_metrics.ready_count, time_metrics.ready_time_ms,
-          state_metrics.exec_count, time_metrics.exec_time_ms, 
-          state_metrics.block_count, time_metrics.block_time_ms, 
-          state_metrics.suspend_count, time_metrics.suspend_time_ms, 
-          state_metrics.exit_count, time_metrics.exit_time_ms
+          state_metrics.new_count          , time_metrics.new_time_ms, 
+          state_metrics.ready_count        , time_metrics.ready_time_ms,
+          state_metrics.susp_ready_count   , time_metrics.susp_ready_time_ms,
+          state_metrics.exec_count         , time_metrics.exec_time_ms, 
+          state_metrics.blocked_count        , time_metrics.blocked_time_ms, 
+          state_metrics.susp_blocked_count , time_metrics.susp_blocked_time_ms,
+          state_metrics.exit_count         , time_metrics.exit_time_ms
         );
 }
--- kernel/src/handlers/cpu/exit_syscall.h
+++ kernel/src/handlers/cpu/exit_syscall.h
@@ -8,5 +8,6 @@
 
 void exit_process_syscall(uint32_t syscall);
 
+void log_process_metrics(uint32_t pid, t_state_metrics state_metrics, t_time_metrics time_metrics);
 
 #endif
